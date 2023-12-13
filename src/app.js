04967b95835188a05932a8f6b52c7a2ba4e3babf
Siphosihle Nomfanelo Mtshali <106113368+creator-droid@users.noreply.github.com>
2023-12-13 17:41:33 +0200

    created the js file

---
+++ src/app.js
@@ -0,0 +1,58 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector(".Temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector(".descrip");
+  let humidityElement = document.querySelector(".humid");
+  let windSpeedElement = document.querySelector(".win");
+  let timeElement = document.querySelector(".Today");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector(".icon");
+
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = `${Math.round(temperature)} °`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiKey = "39017470c087cfb637tbfb8fo35a36b8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#enterCity");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-city");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Cape Town");
